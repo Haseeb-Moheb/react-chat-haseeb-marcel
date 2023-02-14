922ad5053090e703138d2c6bdd75cf31b89079d3
Marcel Prisacari <marcel.prisacari.com>
2023-02-13 19:05:50 -0500

    addedd models

---
+++ server/README.md
@@ -0,0 +1,1 @@
+# react-chat-marcel-haseeb-new-
---
+++ server/models/room.model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const RoomSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    addedUsers: {
+        type: String,
+        required: true,
+    },
+});
+
+module.exports = mongoose.model("Room", RoomSchema);
+
+/* 
+{
+    "name": "Continental",
+    "description": "No business conducted",
+    "addedUsers": ["John Wick", "Winston", "Ms. Perkins"]
+}
+*/
